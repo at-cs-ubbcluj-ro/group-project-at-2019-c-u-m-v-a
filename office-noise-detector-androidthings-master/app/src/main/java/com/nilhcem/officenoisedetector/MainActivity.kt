89631607bd834b64a907e8b69cf171457ed2d6e6
DAVID-PC\David <oprutadavid@gmail.com>
2019-04-04 10:45:16 +0300

    init project

---
+++ office-noise-detector-androidthings-master/app/src/main/java/com/nilhcem/officenoisedetector/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.nilhcem.officenoisedetector
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName!!
+    }
+
+    private val viewModel by lazy { ViewModelProviders.of(this).get(MainViewModel::class.java) }
+    private val ledStrip by lazy { LedStrip() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate")
+        lifecycle.addObserver(ledStrip)
+
+        viewModel.decibelsLiveData.observe(this, Observer { decibels ->
+            Log.i(TAG, "Decibels: ${String.format(Locale.getDefault(), "%.2f", decibels!!)}")
+            ledStrip.showDecibels(decibels)
+        })
+    }
+}
